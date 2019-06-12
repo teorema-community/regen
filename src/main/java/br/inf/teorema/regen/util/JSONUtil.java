ea8d5826088ef9ddfd588fda8b63df41b4ef188d
Samuel Alles Remlinger <samu_65@hotmail.com>
2019-06-12 09:33:33 -0300

    0000270155 >> métodos de JSONUtil colocados como static

--- src/main/java/br/inf/teorema/regen/util/JSONUtil.java
+++ src/main/java/br/inf/teorema/regen/util/JSONUtil.java
@@ -9,29 +9,29 @@ import java.util.Map;
 
 public class JSONUtil {
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
-    public String stringify(Object value) throws JsonProcessingException {
+    public static String stringify(Object value) throws JsonProcessingException {
         return objectMapper.writeValueAsString(value);
     }
 
-    public String prettify(Object value) throws JsonProcessingException {
+    public static String prettify(Object value) throws JsonProcessingException {
         return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(value);
     }
 
-    public <T> T read(String value, Class<T> clazz) throws IOException {
+    public static <T> T read(String value, Class<T> clazz) throws IOException {
         return objectMapper.readValue(value, clazz);
     }
 
-    public Map<String, Object> read(String value) throws IOException {
+    public static Map<String, Object> read(String value) throws IOException {
         return objectMapper.readValue(value, new HashMap<String, Object>().getClass());
     }
 
-    public String prettifyJSONString(String value) throws IOException {
+    public static String prettifyJSONString(String value) throws IOException {
         return prettify(read(value));
     }
 
-    public <T> T convert(Object value, Class<T> clazz) {
+    public static <T> T convert(Object value, Class<T> clazz) {
         return objectMapper.convertValue(value, clazz);
     }
 
