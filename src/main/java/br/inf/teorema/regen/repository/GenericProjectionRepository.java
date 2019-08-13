f772a01eb6511f8de7efbbc223cce3821986e5e9
Samuel Alles Remlinger <samu_65@hotmail.com>
2019-08-13 15:59:29 -0300

    0000286102 >> correção count com distinct

--- src/main/java/br/inf/teorema/regen/repository/GenericProjectionRepository.java
+++ src/main/java/br/inf/teorema/regen/repository/GenericProjectionRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
+import br.inf.teorema.regen.model.SelectAndWhere;
+
 import java.util.List;
 import java.util.Map;
 
 public interface GenericProjectionRepository<T> {
-    public Page<Map<String, Object>> findAllBySpecificationAndProjections(Specification<T> specification, List<String> projections, Pageable pageable, Class<T> clazz) throws NoSuchFieldException;
+    public Page<Map<String, Object>> findAllBySpecificationAndProjections(Specification<T> specification, SelectAndWhere selectAndWhere, Pageable pageable, Class<T> clazz) throws NoSuchFieldException;
 }
